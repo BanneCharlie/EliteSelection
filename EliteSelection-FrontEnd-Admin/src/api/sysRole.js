30712e0ea3f9e150877eba21826c26c555521af6
1876485877@qq.com <bannecharlie@gmail.com>
2024-01-28 21:20:14 +0800

    后台管理系统 权限管理前端页面

---
+++ EliteSelection-FrontEnd-Admin/src/api/sysRole.js
@@ -0,0 +1,65 @@
+import request from '@/utils/request'
+
+const base_api = 'admin/system/sysRole'
+// 分页查询角色数据
+export const GetSysRoleListByPage = (pageNum , pageSize , queryDto) => {
+    return request({
+        url:   `${base_api}/findByPage/${pageNum}/${pageSize}`,// 路径
+        method: 'post',// 提交方式
+        // 接口@RequestBody 前端 data:名称,以json格式传输
+        // 接口没有注解 前端params: 名称
+        data: queryDto
+    })
+}
+
+// 添加角色请求方法
+export const SaveSysRole = (data) => {
+    return request({
+        url: '/admin/system/sysRole/saveSysRole',
+        method: 'post',
+        data
+    })
+}
+
+// 保存修改
+export const UpdateSysRole = (data) => {
+    return request({
+        url: '/admin/system/sysRole/updateSysRole',
+        method: 'put',
+        data
+    })
+}
+
+// 删除角色
+export const DeleteSysRoleById = (roleId) => {
+    return request({
+        url: '/admin/system/sysRole/deleteById/' + roleId,
+        method: 'delete'
+    })
+}
+
+
+// 查询所有的角色数据
+export const GetAllRoleList = (userId) => {
+    return request({
+        url: '/admin/system/sysRole/findAllRoles/' + userId,
+        method: 'get'
+    })
+}
+
+// 查询指定角色所对应的菜单id
+export const GetSysRoleMenuIds = (roleId) => {
+    return request({
+        url: "/admin/system/sysRoleMenu/findSysRoleMenuByRoleId/"+ roleId,
+        method: 'get'
+    })
+}
+
+// 根据角色分配菜单请求方法
+export const DoAssignMenuIdToSysRole = (assignMenuDto) => {
+    return request({
+        url: "/admin/system/sysRoleMenu/doAssign",
+        method: 'post',
+        data: assignMenuDto
+    })
+}
